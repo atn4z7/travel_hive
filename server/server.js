3a532aefd9bd67409371e8bfb23f63fd2eb575da
James Vaughn <jvaughn575@gmail.com>
2018-04-07 14:57:20 -0500

    Added changes to server.js in ./server folder to Only try and load routes if the sequel database was successfully connected to.

--- server/server.js
+++ server/server.js
@@ -13,80 +13,88 @@ import {InspirationRouter} from './routes/inspirationRouter';
 
 export const app = express();
 export const httpServer = require('http').createServer(app);
-const port = process.env.PORT || 3001;
+
+/* 8080 Port is the default for Openshift deployment */
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+      ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const env = process.env.node_env;
 
 (async function(){
-  let mysqlDB = await connectToMysqlDB();
-  let userModel = await UserModel(mysqlDB.sequalizeDB);  
-  let inspirationModel = await InspirationModel(mysqlDB.sequalizeDB);
-  let passport = await passportStrat(userModel);
+  let mysqlDB = await connectToMysqlDB().then((value) =>{return value}, (err) => {return null});
 
-  app.use(bodyParser.json());
+  if(mysqlDB){
+    let userModel = await UserModel(mysqlDB.sequalizeDB);  
+    let inspirationModel = await InspirationModel(mysqlDB.sequalizeDB);
+    let passport = await passportStrat(userModel);
 
-  // Passport requirements
-  // Not secure must change before production
-  app.use(session({ 
-    secret: 'jk_travelhive',
-    resave: false,
-    saveUninitialized: false,
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
+    app.use(bodyParser.json());
 
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
-    
-    /******* Change below line to restrict to our website origin *********/    
-    res.header('Access-Control-Allow-Origin', req.headers.origin);    
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-   });
+    // Passport requirements
+    // Not secure must change before production
+    app.use(session({ 
+      secret: env.EXPRESS_SECRET || 'jk_travelhive',
+      resave: false,
+      saveUninitialized: false,
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
 
-  // Router
-  const router = express.Router();
-
-  /*********Debug Logging middleware***********/
-  if(env === 'debug'){
-    app.use((req, res, next) => {
-      const status = req.isAuthenticated() ? 'logged in' : 'logged out';
-      console.log(
-        'status:', status, '\n',
-        req.sessionStore,
-        req.sessionID,
-        req.session
-      );
+    app.use(function (req, res, next) {
+      res.header('Access-Control-Allow-Credentials', true);
+      
+      /******* Change below line to restrict to our website origin *********/
+      res.header('Access-Control-Allow-Origin', req.headers.origin);    
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       next();
     });
 
-    let showRequest = (req,res,next) => {
-      console.log(req.session);
-      next();
-    }
-  }
-  /****************************************** */
+    // Router
+    const router = express.Router();
 
-   /*************** Helper middleware *******************/
-    const userAuthenticated = (req, res, next) => {
-      if (req.isAuthenticated()) {
-        return next();
+    /*********Debug Logging middleware***********/
+    if(env === 'test'){
+      app.use((req, res, next) => {
+        const status = req.isAuthenticated() ? 'logged in' : 'logged out';
+        console.log(
+          'status:', status, '\n',
+          req.sessionStore,
+          req.sessionID,
+          req.session
+        );
+        next();
+      });
+
+      let showRequest = (req,res,next) => {
+        console.log(req.session);
+        next();
       }
-      res.sendStatus(401);
-    } 
-   /****************************************** */
+    }
+    /****************************************** */
 
-  /*************** Routes*******************/  
-  app.use('/',ApiVersionRouter(router,passport));
-  app.use('/',UserLoginRouter(router,passport));
-  app.use('/',ProfileRouter(router, passport,userAuthenticated));
-  app.use('/',InspirationRouter(router, passport, inspirationModel, userAuthenticated));
-  /*****************************************/
-      
-  // Register all routes with api prefix
-  app.use('/api', router);
+    /*************** Helper middleware *******************/
+      const userAuthenticated = (req, res, next) => {
+        if (req.isAuthenticated()) {
+          return next();
+        }
+        res.sendStatus(401);
+      } 
+    /****************************************** */
+
+    /*************** Routes*******************/  
+    app.use('/',ApiVersionRouter(router,passport));
+    app.use('/',UserLoginRouter(router,passport));
+    app.use('/',ProfileRouter(router, passport,userAuthenticated));
+    app.use('/',InspirationRouter(router, passport, inspirationModel, userAuthenticated));
+    /*****************************************/
+        
+    // Register all routes with api prefix
+    app.use('/api', router);
+
+  }
   
   /* Needed for test otherwise sequelize can't find the database tables */
   if(env === 'test'){
@@ -98,8 +106,8 @@ const env = process.env.node_env;
     });
   /**********************************************************************/   
   } else {    
-    httpServer.listen(port);
+    httpServer.listen(port,ip);
     logger('Api Started','info');    
-    console.log('Express api listening on port ' + port );
+    console.log('Express api listening on http://%s:%s', ip,port);
   }
 })()
